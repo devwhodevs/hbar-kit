d7c69ee92aac97f3ac19c64d171c2e758efa53f0
Oleksandr Ostrovskyi <devwhodevs@gmail.com>
2026-05-30 20:28:04 +0300

    feat(mirror): public barrel and build verification

--- packages/mirror/src/index.ts
+++ packages/mirror/src/index.ts
@@ -1,1 +1,8 @@
-export {}
+export { createMirrorClient } from "./client.js"
+export type { MirrorClient, MirrorClientConfig } from "./client.js"
+export { http } from "./transport.js"
+export type { Transport, TransportOptions } from "./transport.js"
+export { paginate } from "./paginate.js"
+export { normalizeTransaction, normalizeToken, normalizeAccountBalance, isNotFound } from "./normalize.js"
+export type { FindTransactionsParams } from "./resources/transactions.js"
+export * from "./types.js"
